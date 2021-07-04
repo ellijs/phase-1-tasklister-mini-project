9bff30debe71ed9bdf6b5a691df8d61044f425fe
ellijs <ellijs@hotmail.com>
2021-07-04 18:49:40 -0400

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+function updateList(obj) {
+  let newList = document.createElement("li");
+  let removeBtn = document.createElement("button");
+  newList.textContent = obj.newTaskDescription;
+  removeBtn.textContent = "X";
+  newList.append(removeBtn);
+
+  document.querySelector("#tasks").append(newList);
+
+  // Remove Event
+  removeBtn.addEventListener("click", () => newList.remove());
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document
+    .querySelector("#create-task-form")
+    .addEventListener("submit", (e) => {
+      e.preventDefault();
+      // console.log(e);
+      let newTask = {
+        newTaskDescription: e.target.newTaskDescription.value,
+      };
+      updateList(newTask);
+      document.forms[0].reset();
+    });
 });
